5ce799090a5f2e194241951d63dbc53078d18440
luckys-lnz <lalucky22@gmail.com>
2025-04-28 23:46:34 +0000

    Add drizzle configuration for database connection and migrations

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export default {
+  schema: './lib/db/schema.ts',
+  out: './lib/db/migrations',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config 
