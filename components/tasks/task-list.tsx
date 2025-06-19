805a1385763ebcbf5c5f05cacdeaf00a72a96151
luckys-lnz <lalucky22@gmail.com>
2025-06-19 09:59:23 +0000

    Refactor import statement for Task type to use the correct path from lib/types

--- components/tasks/task-list.tsx
+++ components/tasks/task-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Task } from "@/types/task";
+import type { Task } from "@/lib/types";
 import { TaskCard } from "./task-card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
